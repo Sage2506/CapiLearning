42ab26895cfdd5f2f052ac2568641e99faa9eefc
German Eduardo Salazar Aranda <german@agricompra.com>
2022-11-30 07:19:06 -0700

    redux implemented

--- components/home.tsx
+++ components/home.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Button, Text, View } from 'react-native';
+import { connect } from 'react-redux';
+import { setConcepts } from '../actions/concept';
 
-const Home = ({navigation}) => {
+
+const Home = (props) => {
+  const {concepts, navigation} = props
+
+  function increaseConcepts (){
+    props.setConcepts(concepts + [1]);
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>
         New function component! Home 🎉
+      </Text>
+      <Text>
+        Concepts length {concepts.length}
       </Text>
       <Button
         title='Go to collection component'
@@ -13,9 +25,25 @@ const Home = ({navigation}) => {
           navigation.navigate('Collection', { name: 'Jane' })
         }
         />
-
+      <Button
+      title='increase concepts'
+      onPress={() => increaseConcepts()}
+      />
     </View>
   );
 };
 
-export default Home;
+const mapStateToProps = store => ({
+  concepts : store.concept.concepts
+});
+
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setConcepts: data => { dispatch (setConcepts(data))}
+    }
+  }
+;
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- reducers/concept.js
+++ reducers/concept.js
@@ -1,14 +1,16 @@
 import { SET_CONCEPTS } from "../constants/concept"
 
 const initialState = {
-  concepts = []
+  concepts : []
 };
 
-export const conceptReducer = ( state = initialState, action) => {
-  switch(action.type){
-    case SET_CONCEPTS :
-      return {...state,  concepts:action.data}
+export const conceptReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_CONCEPTS:
+      return { ...state, concepts: action.data };
+    default: return state;
   }
+
 }
 
 export default conceptReducer;
